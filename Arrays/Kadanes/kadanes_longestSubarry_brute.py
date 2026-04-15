7e2a8b161beda477acb6e6fa4e0071dd542cb471
tek.ninja <ninjatekpro@gmail.com>
2026-04-15 21:28:15 +0530

    Brute approach of Kadanes

--- Arrays/Kadanes/kadanes_longestSubarry_brute.py
+++ Arrays/Kadanes/kadanes_longestSubarry_brute.py
@@ -1,1 +1,10 @@
 ''' Brute Force Approach'''
+def bruteForce(nums):
+    maxSum = nums[0]
+
+    for i in range(len(nums)):
+        curSum = 0
+        for j in range(i, len(nums)):
+            curSum += nums[j]
+            maxSum = max(maxSum, curSum)
+    return maxSum
